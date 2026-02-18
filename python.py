307564213de8c2455017e24a8ce4b6db061ccf35
Mpstme Student <pawar.swapnil59@gmail.com>
2026-02-18 09:34:35 +0530

    Python file changed

--- python.py
+++ python.py
@@ -1,10 +1,10 @@
 import math
 
 # Input radius
-radius = 2.5
+radius = 3.5
 
 # Calculate area
 area = math.pi * (radius ** 2)
 
 # Print result
-print(f"The area of the circle is: {area:.2f}")
+print(f"The area of this circle is: {area:.2f}")
